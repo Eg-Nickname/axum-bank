d2ca52eb25c11faaec3707238ff39e4ca64a70ff
Alt+F4 <mail@example.com>
2024-03-10 00:16:38 +0100

    Login and signup page register

--- src/pages/login_page.rs
+++ src/pages/login_page.rs
@@ -8,35 +8,38 @@ pub fn LoginPage(
 ) -> impl IntoView {
 
     view! {
-            <ActionForm action=action>
-                <h2 class="title">"Zaloguj się"</h2>
-                    <div class="input-box1">
-                        <i class="fas fa-user"></i>
-                        " Login"
-                        <input type="text" placeholder="Login" maxlength="32" name="username" class="text-input" />
-                    </div>
+            <div class="login-container">
+                <ActionForm action=action>
+                    <h2 class="title">"Zaloguj się"</h2>
+                        <div class="input-box1">
+                            <i class="fas fa-user"></i>
+                            " Login"
+                            <input type="text" placeholder="Login" maxlength="32" name="username" class="text-input" />
+                        </div>
 
-                    <div>
-                        <i class="fas fa-lock"></i>
-                        " Hasło"
-                        <input type="password" placeholder="Hasło" name="password" class="text-input" />
-                    </div>
+                        <div>
+                            <i class="fas fa-lock"></i>
+                            " Hasło"
+                            <input type="password" placeholder="Hasło" name="password" class="text-input" />
+                        </div>
 
 
-                    <div>
-                        "Zapamiętaj mnie"
-                        <input type="checkbox" name="remember" class="text-input" />
-                    </div>
+                        <div>
+                            "Zapamiętaj mnie"
+                            <input type="checkbox" name="remember" class="text-input" />
+                        </div>
+                        <div>
+                            <input type="submit" class="solid-purple-button" value="Zaloguj sie" />
+                        </div>
+                </ActionForm>
+            </div>
 
-                    <input type="submit" class="solid-purple-button" value="Zaloguj sie" />
-            </ActionForm>
-
-            <div>
+            <div class="additional-info-container">
                 <h3>"Jestes tu nowy?"</h3>
-                <p>
+                <p style="margin-bottom: 30px;">
                     "Zarejestruj się już teraz klikając poniższy przycisk. Odniesiesz wiele korzyści zaczynając od możliwości sprawdzaenia swojego salda, až po dostęp do wymiany swoich pieniędzy na inne waluty czy trade'owanie się z innymi użytkownikami!"
                 </p>
-                <A class="transparent-purple-button" href="/signup/">"Zarejestruj sie"</A>  
+                <A  class="transparent-purple-button" href="/signup/">"Zarejestruj sie"</A>  
             </div>
     }
 }
--- src/pages/signup_page.rs
+++ src/pages/signup_page.rs
@@ -7,33 +7,43 @@ pub fn SignupPage(
     action: Action<Signup, Result<(), ServerFnError>>,
 ) -> impl IntoView {
     view! {
-        <ActionForm action=action>
-            <h2>"Zarejestruj się"</h2>
-            <div>
-                <i class="fas fa-user"></i>
-                " Login"
-                <input type="text" placeholder="Login" maxlength="32" name="username" class="auth-input" />
-            </div>
+        <div class="register-container">
+            <ActionForm action=action>
+                <h2>"Zarejestruj się"</h2>
+                <div>
+                    <i class="fas fa-user"></i>
+                    " Login"
+                    <input type="text" placeholder="Login" maxlength="32" name="username" class="auth-input" />
+                </div>
 
-            <div>
-                <i class="fas fa-lock"></i>
-                " Hasło"
-                <input type="password" placeholder="Hasło" name="password" class="auth-input" />
-            </div>
+                <div>
+                    <i class="fas fa-lock"></i>
+                    " Hasło"
+                    <input type="password" placeholder="Hasło" name="password" class="auth-input" />
+                </div>
 
-            <div>
-                <i class="fas fa-lock"></i>
-                " Potwierdź hasło"
-                <input type="password" placeholder="Hasło" name="password_confirmation" class="auth-input" />
-            </div>
+                <div>
+                    <i class="fas fa-lock"></i>
+                    " Potwierdź hasło"
+                    <input type="password" placeholder="Hasło" name="password_confirmation" class="auth-input" />
+                </div>
 
 
-            <div>
-            "Zapamiętaj mnie"
-                <input type="checkbox" name="remember" class="auth-input" />
-            </div>
-
-            <input type="submit" class="solid-purple-button" value="Rejestruj" name="transaction" />
-        </ActionForm>
+                <div>
+                "Zapamiętaj mnie"
+                    <input type="checkbox" name="remember" class="auth-input" />
+                </div>
+                <div>
+                    <input type="submit" class="solid-purple-button" value="Rejestruj" name="transaction" />
+                </div>
+            </ActionForm>
+        </div>
+        <div class="additional-info-container">
+            <h3>"Masz już konto?"</h3>
+            <p style="margin-bottom: 30px;">
+                "Zaloguj się klikając w link poniżej i ciesz się możliwościami!"
+            </p>
+            <A  class="transparent-purple-button" href="/login/">"Zaloguj sie"</A>  
+        </div>
     }
 }
